1dd9e522cf38473c5dd4d9b9b0d685581a2471ff
arkmadj <ahmadjinadu@gmail.com>
2023-06-02 21:49:56 +0100

    added main page

---
+++ src/pages/MailPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-icon name="style" size="10em" />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
