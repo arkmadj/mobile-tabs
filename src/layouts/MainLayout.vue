eb7da708ecf87295d2b082b3f64c2b507e98d197
arkmadj <ahmadjinadu@gmail.com>
2023-06-02 21:44:14 +0100

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white"
+      ><q-tabs>
+        <q-route-tab icon="mail" to="/mails" exact />
+        <q-route-tab icon="alarm" to="/alarms" exact />
+      </q-tabs>
+    </q-footer>
+  </q-layout>
+</template>
