2237c74dc7587a4c2263a54089999740641d60cd
arkmadj <ahmadjinadu@gmail.com>
2023-06-02 22:13:31 +0100

    added document comments

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,6 +3,8 @@ const routes = [
     path: "/",
     component: () => import("layouts/MainLayout.vue"),
     children: [
+      // These are the routes to all pages on the app.
+      // Check the pages folder
       { path: "", component: () => import("pages/IndexPage.vue") },
       { path: "/mails", component: () => import("pages/MailPage.vue") },
       { path: "/alarms", component: () => import("pages/AlarmPage.vue") },
